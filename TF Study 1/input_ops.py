4f537af3d2513f3ec94bb30b013bc4ccdfc0b9a1
jwkangmacpro2 <jwkang10@gmail.com>
2018-06-17 21:46:01 +0900

    (review1): the first review

--- TF Study 1/input_ops.py
+++ TF Study 1/input_ops.py
@@ -1,3 +1,8 @@
+#-*- coding: utf-8 -*-
+#! /usr/bin/env python
+# ------------
+# 한글 주석을 달려면 위 전처리기 필요
+
 # 사용 모델 인풋 형식에 맞게 변환 (여러 데이터를 쉽게 바꿔끼고 섞을 수 있어야한다.)
 
 from sklearn.model_selection import train_test_split
